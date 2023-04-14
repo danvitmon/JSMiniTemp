f7cf18879f3039974345dfeecee4c0843df70454
Daniel Monastirsky <danvitmon@gmail.com>
2023-04-14 14:58:37 -0700

    finished

--- js/site.js
+++ js/site.js
@@ -2,36 +2,31 @@
 function getValues() {
   // get the user's input
   // decide what to do with it
-  let userInput = document.getElementById('message').value;
+  let userInput = document.getElementById('message').value.toLowerCase();
+  
+  userInput = userInput.replace(/[^a-z0-9-]/g, '');
 
-  let reversedInput = reverseString(userInput);
+  let inputPalindrome = checkForPalindrome(userInput);
 
-  displayString(reversedInput);
+  displayPalindrome(inputPalindrome);
 }
 
 // Business logic
-function reverseString(message) {
-  // take a string, return it in reverse
-  let reversedMessage = '';
-
-  for (let index = message.length - 1; index >= 0; index = index - 1) {
-    reversedMessage += message[index];
-  }
-
-  return reversedMessage;
+function checkForPalindrome(message) {
+  return message == message.split('').reverse().join('');
 }
 
 // View
-function displayString(reversedMessage) {
+function displayPalindrome(messagePalindrome) {
   // show the string on the page
-  document.getElementById('msg').textContent = reversedMessage;
-  document.getElementById('alert').classList.remove('d-none');
-}
+  document.getElementById('msg').textContent = messagePalindrome;
 
-// Swal.fire(
-//  {
-//    backdrop: false,
-//    title: 'Rewind',
-//    text: msg
-//  }
-// );
+
+  if(messagePalindrome !== false) {
+    document.getElementById('alertCorrect').classList.remove('d-none');
+    document.getElementById('alertIncorrect').classList.add('d-none');
+} else {
+    document.getElementById('alertIncorrect').classList.remove('d-none');
+    document.getElementById('alertCorrect').classList.add('d-none');
+}
+}
